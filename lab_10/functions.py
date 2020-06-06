8f353c712e4b1612ec913e549413d7d64d5b8d87
Justarone <p_perestoronin@mail.ru>
2020-06-07 00:48:15 +0300

    Minor fix

--- lab_10/functions.py
+++ lab_10/functions.py
@@ -2,5 +2,5 @@ from math import sin, cos, pi
 funcs = list()
 
 funcs.append(lambda x, z: sin(x) * sin(z))
-funcs.append(lambda x, z: sin(cos(x)) * sin(z)))
+funcs.append(lambda x, z: sin(cos(x)) * sin(z))
 funcs.append(lambda x, z: cos(x) * z / 3)
